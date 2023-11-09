4f5b0a8590781a3f72b592efa050c491cb1e0772
Avontss <jorgemchan@gmail.com>
2023-11-09 10:44:47 -0300

    Crud de Temas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,13 +21,14 @@ function Navbar() {
 
                 <div className="container flex justify-between text-lg">
                     <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
-                    
+
                     <div className='flex gap-4'>
                         Postagens
-                        Temas
-                        Cadastrar tema
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
                         Perfil
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+
                     </div>
                 </div>
             </div>
